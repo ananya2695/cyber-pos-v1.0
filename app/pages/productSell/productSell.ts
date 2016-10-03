cdf1ae9175c4f22853eb521b9448e403bd6d0381
niDniDD <oraphan.poo@northbkk.ac.th>
2016-10-03 18:23:02 +0700

    productSell

---
+++ app/pages/productSell/productSell.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/productSell/productSell.html'
+})
+export class ProductSellPage {
+  constructor(public navCtrl: NavController) {
+
+  }
+}
