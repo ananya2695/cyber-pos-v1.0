527d44b3874306b5174ea4b33a9a4447fdce95b8
วัจน์กร จงกลาง < >
2016-10-21 13:56:38 +0700

    image

--- www/build/js/app.bundle.js
+++ www/build/js/app.bundle.js
@@ -155,7 +155,7 @@ var RegisterPage1 = (function () {
         var body = {
             'user_name': user_name, 'user_id': user_id, 'position': position,
             'user_firstname': user_firstname, 'user_lastname': user_lastname,
-            'password': password, 'confirmpassword': confirmpassword, "img_user": "../image/default.png"
+            'password': password, 'confirmpassword': confirmpassword, "img_user": "../image/default.jpg"
         };
         this.viewController.dismiss(body);
         console.dir(body);
@@ -207,7 +207,7 @@ var EditUser = (function () {
         var body = {
             '_id': this._id, 'user_name': user_name, 'user_id': user_id, 'position': position,
             'user_firstname': user_firstname, 'user_lastname': user_lastname,
-            'password': password, 'confirmpassword': confirmpassword, "img_user": "../image/default.png"
+            'password': password, 'confirmpassword': confirmpassword, "img_user": "../image/default.jpg"
         };
         console.log(body);
         this.viewController.dismiss(body);
